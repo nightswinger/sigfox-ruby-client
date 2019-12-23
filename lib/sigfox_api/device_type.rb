3faf3a396898270c0f6058f068225094e846e859
nightswinger <stardustkids83@gmail.com>
2019-12-23 17:51:33 +0900

    update device_type

--- lib/sigfox_api/device_type.rb
+++ lib/sigfox_api/device_type.rb
@@ -14,12 +14,32 @@ module SigfoxApi
       request(method: :get, endpoint: "/v2/device-types/#{id}")
     end
 
+    def update_device_type(id, params)
+      request(method: :put, endpoint: "/v2/device-types/#{id}", params: params)
+    end
+
+    def delete_device_type(id)
+      request(method: :delete, endpoint: "/v2/device-types/#{id}")
+    end
+
     def list_callbacks(id)
       request(method: :get, endpoint: "/v2/device-types/#{id}/callbacks")
     end
 
+    def list_callback_errors(id)
+      request(method: :get, endpoint: "/v2/device-types/#{id}/callbacks-not-delivered")
+    end
+
     def create_callback(id, params)
       request(method: :post, endpoint: "/v2/device-types/#{id}/callbacks", params: params)
+    end
+
+    def update_callback(id, callback_id, params)
+      request(method: :put, endpoint: "/v2/device-types/#{id}/callbacks/#{callback_id}", params: params)
+    end
+
+    def delete_callback(id, callback_id)
+      request(method: :delete, endpoint: "/v2/device-types/#{id}/callbacks/#{callback_id}")
     end
   end
 end
