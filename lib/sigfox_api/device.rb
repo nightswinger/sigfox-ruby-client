ac69c4f604fc734d7ea4fe1e313588de0a7e0644
nightswinger <stardustkids83@gmail.com>
2020-06-23 14:51:16 +0900

    add method create_device

--- lib/sigfox_api/device.rb
+++ lib/sigfox_api/device.rb
@@ -6,6 +6,10 @@ module SigfoxApi
       request(method: :get, endpoint: '/v2/devices', params: options)
     end
 
+    def create_device(params)
+      request(method: :post, endpoint: '/v2/devices', params: params)
+    end
+
     def disengage_seq_number(device_id)
       request(method: :post, endpoint: "/v2/devices/#{device_id}/disengage")
     end
